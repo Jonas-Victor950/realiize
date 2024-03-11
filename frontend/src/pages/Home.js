5f6617be8bdfe0f855114ddba1dbe725c2d9036b
Jonas-Victor950 <111525829+Jonas-Victor950@users.noreply.github.com>
2024-03-10 22:02:34 -0300

    Starting content-two

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,5 +1,5 @@
 import "./Home.css";
-import logo from "../images/logoRealiize.png"
+import logo from "../images/logoRealiize.png";
 
 // Components
 import { NavLink, Link } from "react-router-dom";
@@ -16,19 +16,67 @@ import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 
 const Home = () => {
-
   const navigate = useNavigate();
 
   const dispatch = useDispatch();
 
   return (
     <div id="home">
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
+      <div className="content-one"></div>
+      <div className="content-two">
+        <div className="column">
+          <div>
+            <h2 style={{ fontSize: "35px", marginBottom: "0px" }}>
+              DE O PRÓXIMO
+            </h2>
+            <h3
+              style={{
+                fontSize: "69.3px",
+                marginTop: "0px",
+                marginBottom: "0px",
+              }}
+            >
+              PASSO:
+            </h3>
+            <h3 style={{ fontSize: "17.8px", marginTop: "0px" }}>
+              A escolha de responder esse formulário, já te diferencia dos
+              concorrentes!
+            </h3>
+          </div>
+          <div>
+            <h2 style={{ fontSize: "17.6px", fontWeight: "400" }}>
+              NAS PRÓXIMAS 12h NOSSA EQUIPE ENTRARÁ EM CONTATO COM VOCÊ!
+            </h2>
+          </div>
+        </div>
+        <div className="column"></div>
+        <div className="column">
+          <form>
+            <div>
+              <input placeholder="NOME"></input>
+            </div>
+            <div>
+              <input placeholder="TELEFONE"></input>
+            </div>
+            <div>
+              <input placeholder="EMAIL"></input>
+            </div>
+            <div>
+              <input placeholder="EMPRESA"></input>
+            </div>
+            <div>
+              <input placeholder="NICHO"></input>
+            </div>
+            <div>
+              <input placeholder="FATURAMENTO"></input>
+            </div>
+          </form>
+        </div>
+      </div>
+      <div className="content-three"></div>
+      <div className="content-four"></div>
+      <div className="content-five"></div>
+      <div className="content-six"></div>
     </div>
   );
 };
