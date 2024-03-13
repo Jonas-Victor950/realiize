c404c73cb9d93381659b1a535358fc83d13a5f14
Jonas-Victor950 <111525829+Jonas-Victor950@users.noreply.github.com>
2024-03-12 21:24:54 -0300

    Finalizing content-two

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -24,56 +24,68 @@ const Home = () => {
     <div id="home">
       <div className="content-one"></div>
       <div className="content-two">
-        <div className="column">
-          <div>
-            <h2 style={{ fontSize: "35px", marginBottom: "0px" }}>
-              DE O PRÓXIMO
-            </h2>
-            <h3
-              style={{
-                fontSize: "69.3px",
-                marginTop: "0px",
-                marginBottom: "0px",
-              }}
-            >
-              PASSO:
-            </h3>
-            <h3 style={{ fontSize: "17.8px", marginTop: "0px" }}>
-              A escolha de responder esse formulário, já te diferencia dos
-              concorrentes!
-            </h3>
+        <div className="columns">
+          {" "}
+          <div className="column">
+            <div>
+              <h2 style={{ fontSize: "35px", marginBottom: "0px" }}>
+                DE O PRÓXIMO
+              </h2>
+              <h3
+                style={{
+                  fontSize: "69.3px",
+                  marginTop: "0px",
+                  marginBottom: "0px",
+                }}
+              >
+                PASSO:
+              </h3>
+              <h3
+                style={{ fontSize: "17.8px", marginTop: "0px", width: "240px" }}
+              >
+                A escolha de responder esse formulário, já te diferencia dos
+                concorrentes!
+              </h3>
+            </div>
+            <div>
+              <h2
+                style={{
+                  fontSize: "17.6px",
+                  fontWeight: "600",
+                  width: "240px",
+                }}
+              >
+                NAS PRÓXIMAS 12h NOSSA EQUIPE ENTRARÁ EM CONTATO COM VOCÊ!
+              </h2>
+            </div>
           </div>
-          <div>
-            <h2 style={{ fontSize: "17.6px", fontWeight: "400" }}>
-              NAS PRÓXIMAS 12h NOSSA EQUIPE ENTRARÁ EM CONTATO COM VOCÊ!
-            </h2>
+          <div className="column-two">
+            <div id="triangulo-para-direita"></div>
+            <form>
+              <div>
+                <input placeholder="NOME"></input>
+              </div>
+              <div>
+                <input placeholder="TELEFONE"></input>
+              </div>
+              <div>
+                <input placeholder="EMAIL"></input>
+              </div>
+              <div>
+                <input placeholder="EMPRESA"></input>
+              </div>
+              <div>
+                <input placeholder="NICHO"></input>
+              </div>
+              <div>
+                <input placeholder="FATURAMENTO"></input>
+              </div>
+            </form>
           </div>
-        </div>
-        <div className="column"></div>
-        <div className="column">
-          <form>
-            <div>
-              <input placeholder="NOME"></input>
-            </div>
-            <div>
-              <input placeholder="TELEFONE"></input>
-            </div>
-            <div>
-              <input placeholder="EMAIL"></input>
-            </div>
-            <div>
-              <input placeholder="EMPRESA"></input>
-            </div>
-            <div>
-              <input placeholder="NICHO"></input>
-            </div>
-            <div>
-              <input placeholder="FATURAMENTO"></input>
-            </div>
-          </form>
+          <div className="background-image"></div>
         </div>
       </div>
-      <div className="content-three"></div>
+      <div className="content-three"><span>ai</span></div>
       <div className="content-four"></div>
       <div className="content-five"></div>
       <div className="content-six"></div>
