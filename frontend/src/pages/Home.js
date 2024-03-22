8913ce7b3f47a5a1f43a15a812584657471e6edb
Jonas Victor Paiva De Souza <jonas.victor@MLTBR.LOCAL>
2024-03-22 10:48:41 -0300

    Finalizing content-two

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,8 +1,9 @@
 import "./Home.css";
 import logo from "../images/logoRealiize.png";
-import hashtag from "../images/hashtag.png"
-import gusAndBia from "../images/1.png"
-import yellowContentTwo from "../images/amarelo_content_two.png"
+import hashtag from "../images/hashtag.png";
+import gusAndBia from "../images/1.png";
+import yellowContentTwo from "../images/amarelo_content_two.png";
+import purpleContentTwo from "../images/4.png";
 
 // Components
 import { NavLink, Link } from "react-router-dom";
@@ -28,7 +29,7 @@ const Home = () => {
       <div className="content-one">
         <div className="form-group">
           <div>
-            <img src={hashtag} id="hashtag"/>
+            <img src={hashtag} id="hashtag" />
           </div>
           <div>
             <button>QUERO ALAVANCAR MINHA MARCA!</button>
@@ -36,7 +37,7 @@ const Home = () => {
         </div>
         <div className="form-group">
           <div>
-            <img id="gusAndBia" src={gusAndBia}/>
+            <img id="gusAndBia" src={gusAndBia} />
           </div>
           <div className="yellow-down">
             <div>SOLUÇÃO COMPLETA PARA SUA EMPRESA</div>
@@ -48,13 +49,50 @@ const Home = () => {
       <div className="content-two">
         <div>
           <div id="purple-rectangle"></div>
-          <img src={yellowContentTwo} id="image-yellow-content-two"/>
+          <img src={yellowContentTwo} id="image-yellow-content-two" />
         </div>
-        <div>
-
+        <div className="columns">
+          <div className="column-one">
+            <div className="text-one">DÊ O PRÓXIMO</div>
+            <div className="text-two">PASSO</div>
+            <div className="text-three">
+              A escolha de responder esse formulário, já te diferencia dos
+              concorrentes!
+            </div>
+          </div>
+          <div className="column-two">
+            <img src={purpleContentTwo} className="image-one" />
+          </div>
+          <div className="column-three">
+            <form>
+              <div>
+                <input placeholder="NOME COMPLETO"></input>
+              </div>
+              <div>
+                <input placeholder="TELEFONE"></input>
+              </div>
+              <div>
+                <input placeholder="EMAIL"></input>
+              </div>
+              <div>
+                <input placeholder="EMPRESA"></input>
+              </div>
+              <div>
+                <select></select>
+              </div>
+              <div>
+                <select></select>
+              </div>
+              <button>ENVIAR</button>
+            </form>
+            <div className="text-one">NAS PRÓXIMAS 12h NOSSA EQUIPE ENTRARÁ EM CONTATO COM VOCÊ!</div>
+          </div>
         </div>
+        <div id="purple-rectangle2"></div>
       </div>
-      <div className="content-three"></div>
+      <div className="content-three">
+
+      </div>
       <div className="content-four"></div>
       <div className="content-five"></div>
       <div className="content-six"></div>
