bc39bee08655b162906670d35997777eaff0d75c
Jonas-Victor950 <111525829+Jonas-Victor950@users.noreply.github.com>
2024-04-03 20:44:30 -0300

    feat: Util config

---
+++ frontend/src/utils/config.js
@@ -0,0 +1,29 @@
+export const api = "http://localhost:3000";
+
+export const requestConfig = (method, data, token = null, image = null) => {
+  console.log(data, token);
+  let config;
+
+  if (image) {
+    config = {
+      method,
+      body: data,
+      headers: {},
+    };
+  } else if (method === "DELETE" || data === null) {
+    config = {
+      method,
+      headers: {},
+    };
+  } else {
+    config = {
+      method,
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+  }
+
+  return config;
+};
