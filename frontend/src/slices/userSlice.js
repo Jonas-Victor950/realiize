9146f4373e963aba02061dcbb3d88b287749ab17
Jonas-Victor950 <111525829+Jonas-Victor950@users.noreply.github.com>
2024-04-03 20:49:16 -0300

    feat: userSlice(createUser)

---
+++ frontend/src/slices/userSlice.js
@@ -0,0 +1,55 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import userService from "../services/userService";
+
+const initialState = {
+  users: [],
+  user: {},
+  error: false,
+  success: false,
+  loading: false,
+  message: null,
+};
+
+// Create a User
+export const createUser = createAsyncThunk("user/create", async (user) => {
+  const data = await userService.createUser(user);
+
+  // Check for errors
+  if (data.errors) {
+    return thunkAPI.rejectWithValue(data.errors[0]);
+  }
+
+  return data;
+});
+
+export const userSlice = createSlice({
+  name: "publish",
+  initialState,
+  reducers: {
+    resetMessage: (state) => {
+      state.message = null;
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(createUser.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(createUser.fulfilled, (state, action) => {
+        state.loading = false;
+        state.success = true;
+        state.error = null;
+        state.user = action.payload;
+        state.message = "User criado com sucesso";
+      })
+      .addCase(createUser.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+        state.user = null;
+      });
+  },
+});
+
+export const { resetMessage } = userSlice.actions;
+export default userSlice.reducer;
